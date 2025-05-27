21c0f2e0a08faeb121b9019146dcec3a32304875
gaelVinneren <gael@vinneren.com>
2025-05-27 00:39:43 -0600

    Updates

--- assets/index-BrddyK6z.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-E3261QrH.js";const s=()=>e.jsx("div",{children:"Hola buenas tardes soy una Prueba"});export{s as default};
--- assets/index-MctW2MSC.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-E3261QrH.js";const e=()=>a.jsx("div",{children:"Hola buenas tardes soy una Prueba"});export{e as default};
